7c337240ccf6cb988694dd5c8c10bdcb1f05180a
Codey Du <codeydu@hotmail.com>
2020-05-12 11:21:31 +0900

    second commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,8 +9,8 @@ class NavBar extends React.Component {
               <ul className='flex-space-between height-100pct font-family-arial'>
                 <li className='padding-x-10px hover line-height-75px'><a href="#about">ABOUT ME</a></li>
                 <li className='padding-x-10px hover line-height-75px'><a href="#projects">PROJECTS</a></li>
-                <li className='width-100px hover hover-display-dropDownList line-height-75px position-relative text-align'>LANGUAGE
-                  <ul className='dropDownList display-none'>
+                <li className='width-100px hover hover-display-dropDownList line-height-75px position-relative text-align display-none'>LANGUAGE
+                  <ul className='dropDownList'>
                     <li className='hover text-align'>ENGLISH</li>
                     <li className='hover text-align'>中文</li>
                     <li className='hover text-align'>日本語</li>
